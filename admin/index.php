de542d8671a1d34528f0e15a35c2854d8bf927bb
Ultimater <Ultimater@gmail.com>
2013-11-04 15:19:21 -0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,25 @@
 	require APP_DIR.'/functions.php';
 		
 	$status = "";
-	if(isset($_POST['create_post'])){
+	if(isset($_POST['create_post']))
+        {
 		$title = $_POST['title'];
 		$body = $_POST['body'];
 
 		if(!empty($title) || !empty($body)){
-			insert_post($_POST['title'], $_POST['body'], $conn);
-			$status = "Post Inserted";
+			if(insert_post($_POST['title'], $_POST['body'], $conn))
+                        {
+                          $post_id=get_last_insert($conn);
+                          $post_url='../single.php?id='.$post_id;
+                          if($post_id!==false)
+                          {
+			      $status = "Post Inserted<br /><a target=\"blank\" href=\"$post_url\">View Post</a>";
+                          }else{
+			      $status = "Fail to insert then select post";
+                          }
+                        }else{
+			  $status = "Post Insert Failed";
+                        }
 		} else {
 			$status = "Please enter the fields";
 		}
