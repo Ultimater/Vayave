32cfaf2d03fc0a399651659689885e68f71fd584
Ultimater <Ultimater@gmail.com>
2013-11-04 13:23:01 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     define('APP_DIR',dirname(__FILE__));
     require APP_DIR.'/blog.php';
-    require APP_DIR.'functions.php';
+    require APP_DIR.'/functions.php';
 
 	$posts = get_posts('posts', $conn, 10);
 
