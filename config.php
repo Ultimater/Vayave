4e4edc8b6da5e64fae85cc75dfeb58b576efe9ec
Ultimater <Ultimater@gmail.com>
2013-11-04 15:16:55 -0800

    Update config.php

--- config.php
+++ config.php
@@ -5,3 +5,5 @@ $config = array(
 		"password" => "indian",
 		"database" => 'blog'
 	);
+	
+//Also make sure to also adjust the .htaccess file's RewriteBase so it properly denies requests to /views/
