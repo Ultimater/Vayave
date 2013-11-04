16fcbb047f2054998fcfda51fb43d8c0cc67d789
Ultimater <Ultimater@gmail.com>
2013-11-04 13:26:11 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
-
+        if(!defined('APP_DIR'))exit;
 	function get_view($view, $data){
 		if($data){
 			extract($data);
 		}
 
-		$path = $view.".view.php";
-		require("views/layout.php");
+		$path = $view.'.view.php';
+		require(APP_DIR.'/views/layout.php');
 		//require "views/{$view}.view.php";
 	}
 
